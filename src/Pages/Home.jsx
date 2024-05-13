ba8ee1f1c67997d3376672848b7633f423086d84
Ziyardeen Alhassan <zforkah@gmail.com>
2024-05-13 08:24:17 +0100

    All page tags removed and comments margins added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Articles from './Articles'
+
 import RandomArticles from '../components/RandomArticles'
 
 
 const Home = () => {
   return (
     <>
-    <div>Home</div>
-    
     
     <RandomArticles />
     
